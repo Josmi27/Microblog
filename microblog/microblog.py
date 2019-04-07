ccfb797a452b2ceb064dcc2c38a30f7bbd339162
Joshua Smith <30632852+Josmi27@users.noreply.github.com>
2019-04-07 00:10:27 -0700

    Enabled 'flask shell' Functionality
    The flask shell is a command that pre-imports the application instance. The additions made allows configuration in the "shell context", which is a list(or dictionary) of other symbols to pre-import.

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,5 +1,10 @@
-from app import app
+from app import app, db
+from app.models import User, Post
 '''This file serves as a Python script that defines the Flask application instance.'''
 
 #Remember, The Flask application instnce is called app, and is a member of the app package.
 # So here, it is importing the app variable that is a member of the app package.
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
