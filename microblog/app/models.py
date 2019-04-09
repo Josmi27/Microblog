3fd190098915bbd5a54f57bee4a4f2e8f5614f7a
Joshua Smith <30632852+Josmi27@users.noreply.github.com>
2019-04-09 10:31:57 -0700

    Added Profile Page and Avatars
    The following changes were made:
    -Created user profile view function in Routes.py
    -Created user profile template in User.html and Base.html
    -Added user avatar URLs to Models.py
    -Added user avatars to posts in User.html
    -Added 'last seen' feature, where all users can see when someone was last logged in
    -Added 'profile editor' feature, where users can change their username, and write a description for their profile

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from app import db
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 '''
 A model that represents users:
 
@@ -21,6 +22,9 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+  
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -29,6 +33,11 @@ class User(UserMixin, db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'http://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 '''
