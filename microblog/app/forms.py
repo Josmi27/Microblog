5c3bfca3c9b1d8a692143e1b04d2bb2e587ae143
Joshua Smith <30632852+Josmi27@users.noreply.github.com>
2019-04-15 18:49:55 -0700

    Added Submission of Posts Feature and Pagination

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -42,3 +42,8 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username = self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say Something', validators=[
+        DataRequired()])
+    submit = SubmitField('Submit')
